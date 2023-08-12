0090b353fbeb1b19557d00032b149912b255914d
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2023-08-12 19:15:58 +0200

    feat: add code in App and remove unnecessary boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,79 @@
 import { useState } from 'react'
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [display, setDisplay] = useState('')
+  const [currentSound, setCurrentSound] = useState('')
+
+  const baseSRC = `https://s3.amazonaws.com/freecodecamp/drums/`
+  const getSound = (sound: string): string => baseSRC + sound
+
+  const drumKeys = [
+    { drumKey: 'Q', sound: 'Heater-1.mp3' },
+    { drumKey: 'W', sound: 'Heater-2.mp3' },
+    { drumKey: 'E', sound: 'Heater-3.mp3' },
+    { drumKey: 'A', sound: 'Heater-4_1.mp3' },
+    { drumKey: 'S', sound: 'Heater-6.mp3' },
+    { drumKey: 'D', sound: 'Dsc_Oh.mp3' },
+    { drumKey: 'Z', sound: 'Kick_n_Hat.mp3' },
+    { drumKey: 'X', sound: 'RP4_KICK_1.mp3' },
+    { drumKey: 'C', sound: 'Cev_H2.mp3' },
+  ]
+
+  const handleClick = (id: string) => {
+    const { sound } = drumKeys.filter(({ drumKey }) => drumKey === id)[0]
+    setCurrentSound(sound)
+
+    const audio = new Audio(getSound(currentSound))
+    setDisplay(currentSound.split('.')[0])
+    audio.play()
+  }
 
   return (
-    <>
-      <div></div>
-      <h1>Drum Machine</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <main id='drum-machine'>
+      <section className='display-container'>
+        <div
+          className=''
+          id=''>
+          brand name
+        </div>
+        <div
+          className=''
+          id='power'>
+          <button
+            id='power-btn'
+            type='button'>
+            Power Toggle
+          </button>
+        </div>
+        <div
+          className=''
+          id='display'>
+          {display}
+        </div>
+        <div
+          className=''
+          id=''>
+          Control Slider
+        </div>
+        <div
+          className=''
+          id=''>
+          bank
+        </div>
+      </section>
+      <section className='pad-container'>
+        {drumKeys.map(({ drumKey, sound }) => (
+          <button
+            onClick={(e) => handleClick(e.currentTarget.id)}
+            className='drum-pad'
+            key={drumKey}
+            id={drumKey}>
+            <p> {drumKey} </p>
+            <audio src={getSound(sound)}></audio>
+          </button>
+        ))}
+      </section>
+    </main>
   )
 }
 
